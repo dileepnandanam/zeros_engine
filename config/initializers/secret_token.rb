689783603476f2484ccde2c983d36e8118a3e8dd
dileepnandanam <dileepsnandanam@gmail.com>
2013-09-07 16:38:04 +0530

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZerosEngine::Application.config.secret_key_base = '4b9fc881b7708c7e9b2ef851dbccc11aa6218c3b7ebd5602b6a448072793ea41d86740996ed18f743030c846e1adec0692d5aef46dae2304f5b5392f9b9c61d5'
