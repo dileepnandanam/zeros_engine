5cdca9573f13aeef16275009ae3e236313a65220
dileepnandanam <dileepsnandanam@gmail.com>
2013-09-07 17:49:07 +0530

    Add User

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  attr_accessor :new_password
+  #attr_accessible :login
+
+  after_create :create_password
+
+  def authenticate?(pwd)
+  	Digest::SHA1.hexdigest("#{pwd} #{salt}") == password
+  end
+
+  protected
+  def create_password
+    self.salt = Digest::SHA1.hexdigest("#{login}--#{Time.now}")
+    self.password = Digest::SHA1.hexdigest("#{new_password} #{salt}")
+    self.save
+  end
+end
